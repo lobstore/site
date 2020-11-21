c9190fd2cf794b8217f78f039440fce340655bd7
lobstore <tihon12@list.ru>
2020-11-21 21:56:25 +0300

    create one site

---
+++ index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="styles/main.css">
+	<title>Заголовок</title>
+</head>
+<body>
+	<header>
+		<div id="headerInside">
+			<div id="logo"></div>
+			<div id="CompanyName">Заголовок</div>
+			<div id="navWrap">
+				<a href="#">Главная</a>
+				<a href="#">О нас</a>
+				<a href="#">Магазин</a>
+			</div>
+		</div>
+		<div id="HeaderFooter">
+		</div>
+	</header>
+	<div id="ContentBody">
+		<div id="NaviLeft">
+				<a href="#">Зимние куртки</a>
+				<a href="#">Рубашки</a>
+				<a href="#">Джинсы</a>
+		</div>
+		<div id="Content">
+				<div id="Goods">
+					<img src="#">
+					hsdkjadsnasdnak nsdasndkans,d
+					mkwndasndk jwnd,an dkjwnd,ads
+
+				</div>
+				<div id="Goods">
+					<img src="#">
+					hsdkjadsnasdnak nsdasndkans,d
+					mkwndasndk jwnd,an dkjwnd,ads
+
+				</div>
+				<div id="Goods">
+					<img src="#">
+					hsdkjadsnasdnak nsdasndkans,d
+					mkwndasndk jwnd,an dkjwnd,ads
+
+				</div>
+				<div id="Goods">
+					<img src="#">
+					hsdkjadsnasdnak nsdasndkans,d
+					mkwndasndk jwnd,an dkjwnd,ads
+
+				</div>
+				<div id="Goods">
+					<img src="#">
+					hsdkjadsnasdnak nsdasndkans,d
+					mkwndasndk jwnd,an dkjwnd,ads
+
+				</div>
+		</div>
+	</div>
+</body>
+</html>
